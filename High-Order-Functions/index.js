e248b4d4e20ed871800eafb67ecaeb83e1682f2a
Daniel Mocan <contact_daniel@yahoo.com>
2019-04-23 23:27:44 +0300

    added employees array

--- High-Order-Functions/index.js
+++ High-Order-Functions/index.js
@@ -2,3 +2,26 @@
 const values = [ 0, 2, "text", "Fortech", true, 4, 10, "fp", "vasile", 999 ];
 
 // 2. Sum the salaries of Fortech employees that are working for HR department, please use filter, reduce, map. Before we sum the salaries, lets give the HR department a raise, 20% each.
+const employees = [
+    {
+      fullName: "Sandra",
+      department: "Development",
+      salary: 8000,
+    },
+    {
+      fullName: "Rodica",
+      department: "HR",
+      salary: 5000,
+    },
+    {
+      fullName: "Alexandra",
+      department: "HR",
+      salary: 3000,
+    },
+    {
+      fullName: "Horea",
+      department: "HR",
+      salary: 3000,
+    },
+  ];
+  
