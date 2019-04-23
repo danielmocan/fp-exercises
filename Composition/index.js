4804f0788d4af9e4186346443b5a36fcfba2e7a9
Daniel Mocan <contact_daniel@yahoo.com>
2019-04-23 23:40:41 +0300

    Updated composition

--- Composition/index.js
+++ Composition/index.js
@@ -1,3 +1,7 @@
 // 1. Its easier for us to undestand if the argumets would be applied from left to right, here comes `pipe` to the rescue.
 // Please create a pipe function that applies the params received from left to right.
+// Using Pipe and the functions bellow, createa a doubleIncrementAndSquare function
 
+const increment = a => a + 1;
+const double = a => a + a;
+const square = a => a * a;
