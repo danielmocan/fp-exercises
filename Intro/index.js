aac7f683adc557c106cb75ab47d3175cc6535bc8
Daniel Mocan <contact_daniel@yahoo.com>
2019-04-24 09:30:02 +0300

    Update index.js

--- Intro/index.js
+++ Intro/index.js
@@ -1,4 +1,6 @@
-// We have the following employees list, because the all employees did a good job in the past year, the management decided to raise the salary of each employee with 15%, please implement using: imperative and declarative styles.
+// We have the following employees list, because the all employees did a good job in the past year,
+//the management decided to raise the salary of each employee with 15%, 
+//please implement using: imperative and declarative styles.
 
 const employees = [
   { firstName: "Marius", lastName: "Vanca", salary: 1000 },
